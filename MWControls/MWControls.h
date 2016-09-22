0ac826b11b6358a63eb7a768a59dcc84bce903b3
gangelo <web.gma@gmail.com>
2016-09-21 22:58:50 -0400

    Initial commit

---
+++ MWControls/MWControls.h
@@ -0,0 +1,19 @@
+//
+//  MWControls.h
+//  MWControls
+//
+//  Created by Gene M. Angelo  Jr. on 9/21/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWControls.
+FOUNDATION_EXPORT double MWControlsVersionNumber;
+
+//! Project version string for MWControls.
+FOUNDATION_EXPORT const unsigned char MWControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWControls/PublicHeader.h>
+
+
